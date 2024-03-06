beaf36212567a27e0c945bd702aec5cc06492619
EthanGilles <ehgilles01@gmail.com>
2024-03-06 16:01:14 -0500

    make it butter

--- scripts.js
+++ scripts.js
@@ -2,21 +2,39 @@ var canvas = document.getElementById('background-canvas');
 var ctx = canvas.getContext('2d');
 ctx.font = "30px Kode Mono";
 
+const totalStars = 400; //Total number of stars on the screen
 const speed = 4; //Max speed of stars on the screen
 const size = 2.5; //Max size of stars on the screen
-const colors = ["#FFFFF0", "#DEFBFF", "#FFEFC4", "#FCE4BD", "#F6F7E6", "#FF9078", "#FFDB78"]; //Color options for stars on the screen
+const colors = ["#FFFFF0", "#DEFBFF", "#FFEFC4", "#FCE4BD", "#F6F7E6", "#FA8787", "#FFDB78", "#7FB0DB", "#668ED4"]; //Color options for stars on the screen
 //Color options are all muted White, Blues, Yellows and Reds.
 
 function randomInt(min, max) {
     return Math.floor(Math.random() * (max - min) ) + min;
 }
 
+function speedToSize(speed) { //maps size to speed for 3d effect
+    return -((speed/size) * speed + size);
+}
+
 class Star {
     constructor() {
         this.init();
     }
-
     init(){
+        this.color = colors[randomInt(0,colors.length)];
+        this.size = Math.random() * size;
+        this.position = {
+            x: randomInt(0,window.innerWidth),
+            y: randomInt(-5, window.innerHeight+5)
+        }
+        this.velocity = {
+            x: (Math.random() * -speed) -0.2,
+            y: 0
+        }
+    }
+    reset(){
+        this.color = colors[randomInt(0,colors.length)];
+        this.size = Math.random() * size;
         this.position = {
             x: window.innerWidth + randomInt(20,100),
             y: randomInt(-5, window.innerHeight+5)
@@ -25,8 +43,6 @@ class Star {
             x: (Math.random() * -speed) -0.2,
             y: 0
         }
-        this.size = Math.random() * size;
-        this.color = colors[randomInt(0,colors.length)];
     }
     draw() {
         ctx.globalAlpha = 0.5;
@@ -34,13 +50,23 @@ class Star {
         ctx.fillStyle = this.color;
         ctx.arc(this.position.x, this.position.y, this.size, 0, Math.PI * 2);
         ctx.fill();
+
+
+        if(Math.random() < 0.8 ) { //provides glow effect
+            ctx.globalAlpha = 0.1;
+            ctx.beginPath();
+            ctx.fillStyle = this.color;
+            ctx.arc(this.position.x, this.position.y, this.size*2, 0, Math.PI * 2);
+            ctx.fill();
+        }
+
     }
     update() {
         this.position.x += this.velocity.x
         this.position.y += this.velocity.y
         ctx.globalCompositionOperation = 'lighter'; //if circles overlap make them lighter
         if(this.isDead()) {
-            this.init();
+            this.reset();
         }
         this.draw();
     }
@@ -50,11 +76,12 @@ class Star {
     }
 }
 
-const totalStars = 300;
+
 let stars = Array.from(Array(totalStars), () => new Star());
 
 function animate() {
-    ctx.fillStyle = "#071330"
+    ctx.fillStyle = "#05061F";
+    ctx.globalAlpha = 0.6;
     ctx.fillRect(0,0,canvas.width,canvas.height);
     stars.forEach(star => star.update());
 
@@ -68,3 +95,4 @@ animate();
 // (CenterX, CenterY, Radius, Start angle, End angle)
 
 
+ 
